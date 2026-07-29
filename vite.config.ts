293d17c9804a32da9ad23cfa5e1e5156f0d92f94
GabrieIPujol <gabriel.pagvelosa@gmail.com>
2026-07-29 13:32:52 -0300

    Ajuste para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", 
+  base: "/Portfolio-V2/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
